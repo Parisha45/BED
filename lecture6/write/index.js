dbb7435eb05dab1d174816928b6204252d02a979
Parisha45 <parishamahajan2@gmail.com>
2025-07-15 10:43:38 +0530

    Lecture6

---
+++ lecture6/write/index.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+
+    fs.writeFile("../demo.txt","hello g27", function(err){
+        if(err) return console.log(err);
+        console.log("success!!")
+    })
+
+    
+    fs.writeFile("../demo2.txt","hello g27", function(err){
+        if(err) return console.log(err);
+        console.log("success!!")
+    })
+    
